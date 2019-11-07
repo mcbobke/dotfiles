bb6a35c5d3286efd5a1770a00dc518943079664d
Matt Bobke <mcbobke@gmail.com>
2019-11-07 14:47:25 -0800

    Updated install script to check for existing repo

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+if [ -d ~/.vim/bundle/Vundle.vim ]
+then
+    OWD=$PWD
+    cd ~/.vim/bundle/Vundle.vim
+    git pull > /dev/null
+    cd $OWD
+    echo Vundle git repo updated
+else
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
 cp -f -t ~ .bashrc .bash_aliases .vimrc .gitconfig
-echo Files copied
+echo Dotfiles copied to home folder
 source ~/.bashrc
