735cd7e12ffe2711eed4025dcfa1b2416b230550
Matt Bobke <mcbobke@gmail.com>
2020-10-27 11:11:31 -0700

    Redirect all output in install

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@ if [ -d ~/.vim/bundle/Vundle.vim ]
 then
     OWD=$PWD
     cd ~/.vim/bundle/Vundle.vim
-    git pull > /dev/null
+    git pull > /dev/null 2>&1
     cd $OWD
     echo Vundle git repo updated
 else
-    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim > /dev/null 2>&1
     echo Vundle git repo cloned to ~/.vim/bundle/Vundle.vim
 fi
 
 if [ ! -d ~/.asdf ]
 then
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0 > /dev/null
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.0 > /dev/null 2>&1
     echo asdf git repo cloned to ~/.asdf
 fi
 
